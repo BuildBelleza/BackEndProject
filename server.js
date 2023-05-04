507f89d446db70fe2c7901546636607116e10988
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-05-04 04:08:49 -0400

    add server file code and nav bar code

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const server = express();
+
+server.engine('html', es6Renderer);
+server.set('views', 'views');
+server.set('view engine', 'html');
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', (req, res) => {
+    res.render('index', {
+        partials:    
+            setMainView('landing')
+    });
+}); 
+
+server.get('/heartbeat', (req, res) => {
+    res.json({ 'is': 'working',
+    "status": "good" })
+});
+
+server.listen(PORT, () => console.log(`The server is running on port ${PORT}.`));
